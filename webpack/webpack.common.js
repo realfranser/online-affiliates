ea971fc6489879b9a15badf5ebff9bcd16d1d6be
realfranser <serranoarresejavier@gmail.com>
2022-02-07 23:52:34 +0100

    own webpack config

---
+++ webpack/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.js"),
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  stats: "errors-only",
+};
